649a7dcba0c494cb3aa2ad7f3eac6764f0d94723
tjdans34568 <tjdans34568@gmail.com>
2022-01-10 16:27:27 +0900

    reflection test onready

--- src/test.java
+++ src/test.java
@@ -1,8 +1,12 @@
-
+import classPackage.Dog;
 
 public class test {
 
     public static void main(String[] args) {
+
+        Dog dog = new Dog("BOb",4);
+        Class dogClass = dog.getClass();
+
 
     }
 
